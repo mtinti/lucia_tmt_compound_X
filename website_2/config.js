ee915efa5054a49c14ee06ceb7e5913a9fae81a9
mtinti <michele.tinti@gmail.com>
2021-02-15 17:33:56 +0000

    new website

---
+++ website_2/config.js
@@ -0,0 +1,20 @@
+let X1='SignedRank';
+let Y1='FDR';
+let X2='log10TMT';
+let Y2='SignedRank';
+
+let FLIP_Y1=true;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='Lucia Project';
+
+let margin = 0.05;
+
+let BOX_PLOT_COLS_1 = ['SM1','SM2','SM3']
+let BOX_PLOT_COLS_2 = ['R1','R2','R3']
+
+let CONDITION_1 = 'SM'
+let CONDITION_2 = 'R'
